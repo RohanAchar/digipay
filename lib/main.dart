962641540e0d68fa835fa0166d724dda4c3a785f
Sriharish0604 <rsriharish608@gmail.com>
2020-02-24 20:42:40 +0530

    till dashboard

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:digipay_master1/views/shopping_cart/cartpage.dart';
+import 'package:flutter/material.dart';
+import 'home_widget.dart';
+import 'views/dashboard.dart';
+import 'package:digipay_master1/views/first_view.dart';
+import 'package:digipay_master1/views/sign_up_view.dart';
+import 'package:digipay_master1/widgets/provider_widget.dart';
+import 'package:digipay_master1/services/auth_service.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Provider(
+      auth: AuthService(),
+      child: MaterialApp(
+        title: "Payment App",
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: HomeController(),
+        routes: <String, WidgetBuilder>{
+          '/signUp': (BuildContext context) => SignUpView(authFormType: AuthFormType.signUp),
+          '/signIn': (BuildContext context) => SignUpView(authFormType: AuthFormType.signIn),
+          '/home': (BuildContext context) => HomeController(),
+          '/cart': (context) => CartPage()
+        },
+      ),
+    );
+  }
+}
+
+class HomeController extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final AuthService auth = Provider.of(context).auth;
+    return StreamBuilder<String>(
+      stream: auth.onAuthStateChanged,
+      builder: (context, AsyncSnapshot<String> snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          final bool signedIn = snapshot.hasData;
+          return signedIn ? Dashboard(): FirstView();
+        }
+        return CircularProgressIndicator();
+      },
+    );
+  }
+}
+
