7ca7810112603ddf24888e3e2620165e3d3d0312
Sriharish0604 <rsriharish608@gmail.com>
2020-03-04 00:03:46 +0530

    lil backend left

--- lib/views/wallet/account/manageaccounts/account_tile.dart
+++
@@ -1,3 +0,0 @@
-/*import 'package:flutter/material.dart';
-import 'package:digipay_master1/models/account.dart';
-*/
--- lib/views/wallet/cards/manage_cards.dart
+++ lib/views/wallet/cards/manage_cards.dart
@@ -3,85 +3,10 @@ import 'package:digipay_master1/services/database.dart';
 import 'package:digipay_master1/widgets/provider_widget.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:digipay_master1/views/wallet/account/addaccounts/globals.dart' as global;
+
 
 /*class ManageCards extends StatelessWidget {
-
-   Firestore _firestore = Firestore.instance;
-   String uid;
-   ManageCards(){
-   getUId();
-   }
-
-   Future<String> getUId() async{
-     final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-
-    uid=  (await _firebaseAuth.currentUser()).uid;
-    print(uid);
-   }
-
-  @override
-  Widget build(BuildContext context) {
-
-     return StreamBuilder<QuerySnapshot>(
-        stream: _firestore
-            .collection('users')
-            .document(uid)
-            .collection('cards')
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-             return Text("error");
-            
-          } else {
-            final listElements = snapshot.data.documents;
-            List<CardDetails> conversationList = [];
-            for (var user in listElements) {
-              final String uid = user.data['user_id'];
-              print('$uid');
-
-              final z = CardDetails(
-                friendUID: uid,
-              );
-              conversationList.add(z);
-            }
-            
-            return ListView(
-              children: conversationList,
-            );
-          }
-            print(snapshot.data.documents);
-            return Text('${snapshot.data.documents}');
-
-          }
-
-    
-
-  
-     );
-}
-}
-class CardDetails extends StatelessWidget {
-  final String card_cvv;
-  final String card_exp;
-  final String card_name;
-  final String card_no;
-  
-  Firestore _firestore = Firestore.instance;
-  CardDetails({this.card_cvv, this.card_exp,this.card_name,this.card_no});
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: DatabaseService(uid: uid).userData,
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return Text('');
-        } 
-      
-    );
-  }
-}
-*/
-class ManageCards extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
@@ -114,10 +39,39 @@ class ManageCards extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.only(top: 8.0, bottom: 4.0),
                 child: Row(children: <Widget>[
-                  Text(card['card_name'], style: new TextStyle(fontSize: 30.0),),
+                  Text(card['card_name'], style: new TextStyle(fontSize: 20.0),),
                   Spacer(),
                 ]),
               ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 4.0),
+                child: Row(children: <Widget>[
+                  Text(card['card_exp'], style: new TextStyle(fontSize: 20.0),),
+                  Spacer(),
+                ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 4.0),
+                child: Row(children: <Widget>[
+                  Text(card['card_no'], style: new TextStyle(fontSize: 20.0),),
+                  Spacer(),
+                ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 4.0),
+                child: Row(children: <Widget>[
+                  new RaisedButton(
+                                  child: new Text("Add Money to Wallet",
+                                  style:TextStyle(
+                                    fontSize:18,
+                                    color:Colors.black54,
+                                  )),
+                                  onPressed: (){}//performSubmit,
+                  ),
+                  Spacer(),
+                ]),
+              ),
+
               /*Padding(
                 padding: const EdgeInsets.only(top: 4.0, bottom: 80.0),
                 child: Row(children: <Widget>[
@@ -139,7 +93,230 @@ class ManageCards extends StatelessWidget {
             ],
           ),
         ),
+         
+    ));
+  }
+}*/
+
+class ManageCards extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+    int balance=0;
+    String h;
+    int fb;
+    
+
+ bool validate() {
+    final form = _formKey.currentState; //all the text fields will be set to values
+    form.save(); // to save the form
+    if (form.validate()) {
+      form.save();
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void _addBalance(){
+    if(validate()){
+      global.wallet=global.wallet+balance;
+      fb=global.wallet;
+      print(global.wallet);
+      
+      
+    }
+
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: StreamBuilder(
+        stream: getUsersTripsStreamSnapshots(context),
+        builder: (context, snapshot) {
+          if(!snapshot.hasData) return const Text("Loading...");
+          return new ListView.builder(
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (BuildContext context, int index) =>
+                  buildTripCard(context, snapshot.data.documents[index]));
+        }
       ),
     );
   }
+
+  Stream<QuerySnapshot> getUsersTripsStreamSnapshots(BuildContext context) async* {
+    final uid = await Provider.of(context).auth.getCurrentUID();
+    yield* Firestore.instance.collection('users').document(uid).collection('cards').snapshots();
+  }
+
+  Widget buildTripCard(BuildContext context, DocumentSnapshot card) {
+    return new Container(
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 4.0),
+                child: Row(children: <Widget>[
+                  Text("Card Name: "+card['card_name'], style: new TextStyle(fontSize: 20.0),),
+                  Spacer(),
+                ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 4.0),
+                child: Row(children: <Widget>[
+                  Text("Card Expiry: "+card['card_exp'], style: new TextStyle(fontSize: 20.0),),
+                  Spacer(),
+                ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 4.0),
+                child: Row(children: <Widget>[
+                  Text("Card No: "+card['card_no'], style: new TextStyle(fontSize: 20.0),),
+                  Spacer(),
+                ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 4.0),
+                child: Row(children: <Widget>[
+                  new RaisedButton(
+                                  child: new Text("Add Money to Wallet",
+                                  style:TextStyle(
+                                    fontSize:18,
+                                    color:Colors.black54,
+                                  )),
+                                  onPressed: (){
+                                    showDialog(
+                                      context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    content: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: new InputDecoration(labelText: "Money to add in the wallet"),
+                              onSaved: (val)=>balance=int.parse(val),
+                            ),
+                          ),
+                          
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: RaisedButton(
+                              child: Text("ADD CASH"),
+                              onPressed:(
+                                
+                                 _addBalance),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                });
+
+                                  }//performSubmit,
+                  ),
+                  Spacer(),
+                ]),
+              ),
+
+              /*Padding(
+                padding: const EdgeInsets.only(top: 4.0, bottom: 80.0),
+                child: Row(children: <Widget>[
+                  Text(
+                      "${DateFormat('dd/MM/yyyy').format(trip['startDate'].toDate()).toString()} - ${DateFormat('dd/MM/yyyy').format(trip['endDate'].toDate()).toString()}"),
+                  Spacer(),
+                ]),
+              ),*/
+              /*Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                child: Row(
+                  children: <Widget>[
+                    //Text("\$${(trip['budget'] == null)? "n/a" : trip['budget'].toStringAsFixed(2)}", style: new TextStyle(fontSize: 35.0),),
+                    Spacer(),
+                    Icon(Icons.directions_car),
+                  ],
+                ),
+              )*/
+            ],
+          ),
+        ),
+         
+    ));
+  }
 }
+                /*context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    content: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: new InputDecoration(labelText: "Receiver's email id"),
+                              onSaved: (val)=>h=(val),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: new InputDecoration(labelText: "Amount to be sent"),
+                              onSaved: (val)=>bal=int.parse(val),
+                            ),
+                          ),
+                          
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: RaisedButton(
+                              child: Text("ADD CASH"),
+                              onPressed:(
+                                
+                                 _addBalance),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                });
+
+                                  }//performSubmit,
+                  ),
+                  Spacer(),
+                ]),
+              ),
+
+              /*Padding(
+                padding: const EdgeInsets.only(top: 4.0, bottom: 80.0),
+                child: Row(children: <Widget>[
+                  Text(
+                      "${DateFormat('dd/MM/yyyy').format(trip['startDate'].toDate()).toString()} - ${DateFormat('dd/MM/yyyy').format(trip['endDate'].toDate()).toString()}"),
+                  Spacer(),
+                ]),
+              ),*/
+              /*Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                child: Row(
+                  children: <Widget>[
+                    //Text("\$${(trip['budget'] == null)? "n/a" : trip['budget'].toStringAsFixed(2)}", style: new TextStyle(fontSize: 35.0),),
+                    Spacer(),
+                    Icon(Icons.directions_car),
+                  ],
+                ),
+              )*/
+            ],
+          ),
+        ),
+         
+    ));
+  }
+}
+*/
